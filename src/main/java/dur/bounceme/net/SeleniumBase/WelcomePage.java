98e5fc2cb6349d5c4fee400089b1b53f6b10ac3a
thufir <hawat.thufir@gmail.com>
2018-12-22 11:05:05 -0800

    incremental

--- src/main/java/dur/bounceme/net/SeleniumBase/WelcomePage.java
+++ src/main/java/dur/bounceme/net/SeleniumBase/WelcomePage.java
@@ -4,32 +4,40 @@ import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 class WelcomePage {
-
+    
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private WebDriver webDriver = null;
-
+    
+    @FindBy(partialLinkText = "Books")
+    private WebElement books;
+    
     private WelcomePage() {
     }
-
+    
     WelcomePage(WebDriver webDriver) {
         this.webDriver = webDriver;
         LOG.info(webDriver.getCurrentUrl());
     }
-
+    
     static WelcomePage init(WebDriver webDriver) {
         return new WelcomePage(webDriver);
     }
-
+    
     void populateCatalogue() {
         LOG.info("start..");
-        WebElement foo = webDriver.findElement(By.xpath("//a[contains(text(), 'Books')]"));
-        LOG.info(foo.toString());
-        LOG.info(foo.getText());
-        LOG.info(foo.getTagName());        
-        LOG.info(String.valueOf(foo.isEnabled()));        
+        //  WebElement foo = webDriver.findElement(By.xpath("//a[contains(text(), 'Books')]"));
+        //   LOG.info(foo.toString());
+        //   LOG.info(foo.getText());
+        //   LOG.info(foo.getTagName());        
+        //   LOG.info(String.valueOf(foo.isEnabled()));    
+        
+        LOG.info(books.toString());
+        LOG.info(books.getText());
+        LOG.info(books.getTagName());
         LOG.info("..done");
     }
-
+    
 }
