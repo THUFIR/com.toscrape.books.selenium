1d5ec5dc7ddd27960de6aa373e52f28b21dbb1ea
thufir <hawat.thufir@gmail.com>
2018-12-27 13:13:55 -0800

    incremental

--- src/main/java/dur/bounceme/net/SeleniumBase/HomePage.java
+++ src/main/java/dur/bounceme/net/SeleniumBase/HomePage.java
@@ -1,22 +1,15 @@
 package dur.bounceme.net.SeleniumBase;
 
-import java.util.List;
 import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 class HomePage extends AbstractPage {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private WebDriver webDriver = null;
-
-    @FindBy(partialLinkText = "Books")
-    private WebElement books;
-
-    @FindBy(xpath = "//div[@class='side_categories']/ul/li/ul/li/a")
-    private List<WebElement> links;
 
     private final String sideCategory = "//div[@class='side_categories']/ul/li/ul";
 
@@ -27,13 +20,6 @@ class HomePage extends AbstractPage {
         LOG.fine(webDriver.getCurrentUrl());
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
-    }
-
-    private void populateCatalogue() {
-        links.stream().map((webElement) -> {
-            LOG.fine(webElement.getText());
-            return webElement;
-        });
     }
 
     public void frag() {
