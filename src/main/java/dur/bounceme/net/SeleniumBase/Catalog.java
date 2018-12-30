775109d372273f7c215119c32572309c524682db
thufir <hawat.thufir@gmail.com>
2018-12-30 05:48:10 -0800

    incremental

--- src/main/java/dur/bounceme/net/SeleniumBase/Catalog.java
+++ src/main/java/dur/bounceme/net/SeleniumBase/Catalog.java
@@ -3,8 +3,6 @@ package dur.bounceme.net.SeleniumBase;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,9 +14,6 @@ public class Catalog {
     private WebElement container = null;
     private final WebDriver webDriver = null;
 
-//    @FindBy(xpath = "//ul/li/ul/li/a")
-//    private final List<WebElement> listItems = null;
-//    @FindBy(xpath = "//div[@id='container']")
     @FindBy(xpath = "//li/a")
     List<WebElement> catalogElements = null;
 
@@ -33,15 +28,14 @@ public class Catalog {
 
     public void iterateCatalogElements() {
         LOG.info("trying to iterate..");
-        LOG.info(catalogElements.toString());
-
-        // List<String> f = catalogElements.stream().map(WebElement::getText).collect(Collectors.toList());
+        LOG.fine(catalogElements.toString());
         ListIterator<WebElement> li = catalogElements.listIterator();
+        WebElement listItem = null;
         while (li.hasNext()) {
+            listItem = li.next();
             LOG.info(li.next().getTagName());
-            LOG.info(li.next().getAttribute("href"));
-//            LOG.info(li.next());
+            LOG.info(li.next().getText());
+            Category c = new Category(webDriver, listItem);
         }
-        //   LOG.info(f.toString());
     }
 }
